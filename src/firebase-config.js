9c67ad8351f4a5d035f9ab5a8f1acb54f894d38e
Jibanta <63874888+jibanta66@users.noreply.github.com>
2022-08-17 01:42:35 +0700

    e

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -2,14 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "@firebase/firestore";
 
 const firebaseConfig = { // ID
-  apiKey: "AIzaSyBAM0lqNN02UpmIehJXXWDFOEoCyW7UpTQ",
-  authDomain: "fierbasechat-b1b5d.firebaseapp.com",
-  databaseURL: "https://fierbasechat-b1b5d-default-rtdb.firebaseio.com",
-  projectId: "fierbasechat-b1b5d",
-  storageBucket: "fierbasechat-b1b5d.appspot.com",
-  messagingSenderId: "256763580850",
-  appId: "1:256763580850:web:90d2cb3fdac3e7900e14ab",
-  measurementId: "G-GN4ZT0QKGX"
+
 };
 
 const app = initializeApp(firebaseConfig);// IINITIAL
